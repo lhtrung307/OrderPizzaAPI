de85efa20a6c0e8f68c1f30ad6cd6e59b51361af
pyco <pyco@mbp-0111.pyco.vn>
2019-07-04 12:26:11 +0700

    add kafka to product pricing rule

--- controllers/product-pricing-rule-controllers.js
+++ controllers/product-pricing-rule-controllers.js
@@ -1,6 +1,52 @@
 const ProductPricingRuleServices = require("../services/product-pricing-rule-services");
 const Boom = require("@hapi/boom");
 
+let Kafka = require("node-rdkafka");
+require("dotenv").config();
+
+let kafkaConf = {
+  "group.id": "cloudkarafka-pricingrules",
+  "metadata.broker.list": process.env.CLOUDKARAFKA_BROKERS.split(","),
+  "socket.keepalive.enable": true,
+  "security.protocol": "SASL_SSL",
+  "sasl.mechanisms": "SCRAM-SHA-256",
+  "sasl.username": process.env.CLOUDKARAFKA_USERNAME,
+  "sasl.password": process.env.CLOUDKARAFKA_PASSWORD,
+  debug: "generic,broker,security"
+};
+
+const prefix = process.env.CLOUDKARAFKA_TOPIC_PREFIX;
+const topic = `${prefix}updatePricingRule`;
+const producer = new Kafka.Producer(kafkaConf);
+const maxMessages = 20;
+
+const genMessage = (pricingRule) => {
+  let message = JSON.stringify(pricingRule)
+  return Buffer.from(message);
+}
+
+// producer.on("ready", function(arg) {
+//   console.log(`producer ${arg.name} ready.`);
+//   for (var i = 0; i < maxMessages; i++) {
+//     producer.produce(topic, -1, genMessage(i), i);
+//   }
+//   setTimeout(() => producer.disconnect(), 0);
+// });
+
+producer.on("disconnected", function(arg) {
+  console.log("Sent")
+});
+
+producer.on("event.error", function(err) {
+  console.error(err);
+  process.exit(1);
+});
+producer.on("event.log", function(log) {
+  console.log(log);
+});
+
+
+
 module.exports.list = async (request, h) => {
   let query = request.query;
   let sortType;
@@ -30,6 +76,13 @@ module.exports.create = async (request, h) => {
       pricingRule
     );
     if (createdPricingRule) {
+      producer.connect();
+      producer.on("ready", function(arg) {
+        console.log(`producer ${arg.name} ready.`);
+        producer.produce(topic, -1, genMessage(pricingRule), "key");
+        setTimeout(() => producer.disconnect(), 0);
+      });   
+      console.log("hello")   
       return h.response(createdPricingRule);
     }
     return h.response("Cannot create pricing rule");
